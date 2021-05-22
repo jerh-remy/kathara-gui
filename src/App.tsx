481aa2d9b732d2aeec4aa15c2554bd8fcbc9bdb5
Jeremy Offori <36880398+jerh-remy@users.noreply.github.com>
2021-05-22 16:54:37 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import icon from '../assets/icon.svg';
+import './App.global.css';
+
+// const Hello = () => {
+//   return (
+//     <div>
+//       <div className="Hello">
+//         <img width="200px" alt="icon" src={icon} />
+//       </div>
+//       <h1>electron-react-boilerplate</h1>
+//       <div className="Hello">
+//         <a
+//           href="https://electron-react-boilerplate.js.org/"
+//           target="_blank"
+//           rel="noreferrer"
+//         >
+//           <button type="button">
+//             <span role="img" aria-label="books">
+//               📚
+//             </span>
+//             Read our docs
+//           </button>
+//         </a>
+//         <a
+//           href="https://github.com/sponsors/electron-react-boilerplate"
+//           target="_blank"
+//           rel="noreferrer"
+//         >
+//           <button type="button">
+//             <span role="img" aria-label="books">
+//               🙏
+//             </span>
+//             Donate
+//           </button>
+//         </a>
+//       </div>
+//     </div>
+//   );
+// };
+
+export default function App() {
+  return (
+    <div className="absolute inset-0 bg-white text-center h-full flex flex-col justify justify-center">
+      ERB + TAILWIND = ❤
+    </div>
+    // <Router>
+    //   <Switch>
+    //     <Route path="/" component={Hello} />
+    //   </Switch>
+    // </Router>
+  );
+}
